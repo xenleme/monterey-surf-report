ad81026a070c02757eadb6ccb9f97e478d4adc26
xenleme <xenleme@gmail.com>
2019-10-16 20:19:10 +0300

    Added the ability to fetch the data and then build a chart

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
-import getSpots from './requests';
+import { getSpots, getSpotForecast } from './requests';
+import Chart from 'chart.js';
 
 const spotsEl = document.querySelector('#spots');
+const spotNameEl = document.querySelector('#spot-name');
 
 const addSpots = async () => {
   const spots = await getSpots();
   spotsEl.textContent = spots.join(', ');
 };
 
+const addSpotForecast = async spotId => {
+  const spotForecast = await getSpotForecast(spotId);
+  const spotName = spotForecast[0].spot_name;
+  spotNameEl.textContent = spotName;
+
+  const waveObj = {
+    xLabels: [],
+    yValues: []
+  };
+
+  for (let i = 0; i < spotForecast.length - 1; i++) {
+    waveObj.xLabels.push(spotForecast[i].hour);
+    waveObj.yValues.push(parseFloat(spotForecast[i].size_ft.toFixed(2)));
+  }
+
+  const ctxWave = document.querySelector('#waveHeightChart');
+
+  const waveHeightChart = new Chart(ctxWave, {
+    type: 'bar',
+    data: {
+      labels: waveObj.xLabels,
+      datasets: [
+        {
+          label: 'Wave Height',
+          data: waveObj.yValues,
+          backgroundColor: 'rgba(21, 44, 66, 0.89)',
+          borderColor: 'rgba(216, 219, 222, 0.89)',
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+};
+
 addSpots();
+addSpotForecast(161);
--- src/requests.js
+++ src/requests.js
@@ -1,6 +1,5 @@
-const spotsURL = 'http://api.spitcast.com/api/county/spots/monterey/';
-
 const getSpots = async () => {
+  const spotsURL = 'http://api.spitcast.com/api/county/spots/monterey/';
   const response = await fetch(spotsURL);
 
   if (response.status === 200) {
@@ -11,4 +10,16 @@ const getSpots = async () => {
   }
 };
 
-export default getSpots;
+const getSpotForecast = async spotId => {
+  const spotForecastURL = `http://api.spitcast.com/api/spot/forecast/${spotId}/`;
+  const response = await fetch(spotForecastURL);
+
+  if (response.status === 200) {
+    const data = await response.json();
+    return data;
+  } else {
+    throw new Error('Unable to get spots forecast.');
+  }
+};
+
+export { getSpots, getSpotForecast };
