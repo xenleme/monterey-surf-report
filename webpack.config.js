6e2d7863c1082ba690884ba5d04fbc83d06dd728
xenleme <xenleme@gmail.com>
2019-10-25 09:28:42 +0300

    Added file loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,17 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'img'
+            }
+          }
+        ]
       }
     ]
   },
