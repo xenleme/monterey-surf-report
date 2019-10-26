1c7b49ac0c29939bd57e90d831ce432be3050901
xenleme <xenleme@gmail.com>
2019-10-26 07:54:04 +0300

    Added a loader

--- src/js/index.js
+++ src/js/index.js
@@ -72,6 +72,11 @@ const addSpotForecast = async spotId => {
   createWaveHeightChart(spotForecast, spotNameEl);
 };
 
+window.addEventListener('load', () => {
+  const loader = document.querySelector('.loader');
+  loader.className += ' hidden';
+});
+
 addSpots();
 addWaterTemp();
 addWindSpeed();
